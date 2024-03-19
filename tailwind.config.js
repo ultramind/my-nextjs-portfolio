00f714180caa611e6b999dacaabcbe3063d2e640
ULTRACODE <macbook@MACBOOKs-MacBook-Pro.local>
2024-03-19 23:53:35 +0100

    Configured my tailwind css to the project color palletes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "978px",
+      xl: "1440px",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#FF014F",
+        bgColor: "#dde1e7",
+        textColor: "#dde1e7",
       },
     },
   },
