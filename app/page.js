73123bf57615d6eb0de00ff811d442f436ca5dae
ULTRACODE <macbook@MACBOOKs-MacBook-Pro.local>
2024-03-20 12:36:55 +0100

    Added the Service and Portfolio component

--- app/page.js
+++ app/page.js
@@ -5,8 +5,16 @@ import { useEffect, useState } from "react";
 import Aos from "aos";
 import MobileHeader from "./components/MobileHeader";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+import Testimonial from "./constants/testimonial";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Link from "next/link";
+import { AiOutlineArrowUp } from "react-icons/ai";
 
-export default function Home() {
+export default function Index() {
   const [theme, setTheme] = useState("light");
   const [navEffect, setNavEffect] = useState(false);
 
@@ -36,6 +44,53 @@ export default function Home() {
           theme === "dark" ? "border-gray-800" : "border-gray-300"
         }`}
       />
+      <Services theme={theme} />
+      <hr
+        className={`w-[100%] border-2 ${
+          theme === "dark" ? "border-gray-800" : "border-gray-300"
+        }`}
+      />
+      <Portfolio theme={theme} />
+      <hr
+        className={`w-[100%] border-2 ${
+          theme === "dark" ? "border-gray-800" : "border-gray-300"
+        }`}
+      />
+
+      {/* <Resume theme={theme} />
+      <hr
+        className={`w-[100%] border-2 ${
+          theme === "dark" ? "border-gray-800" : "border-gray-300"
+        }`}
+      />
+      <Testimonial theme={theme} />
+      <hr
+        className={`w-[100%] border-2 ${
+          theme === "dark" ? "border-gray-800" : "border-gray-300"
+        }`}
+      />
+      <Contact theme={theme} />
+      <hr
+        className={`w-[100%] border-2 ${
+          theme === "dark" ? "border-gray-800" : "border-gray-300"
+        }`}
+      />
+      <Footer />
+      <Link
+        to="home"
+        spy={true}
+        smooth={true}
+        offset={-100}
+        duration={500}
+        className={
+          navEffect &&
+          `p-4 ${
+            theme === "dark" ? "dark-theme-shadow-effect" : "shadow-default"
+          } cursor-pointer rounded-full text-primary font-bold text-2xl fixed right-4 bottom-8 z-10 ease-in-out duration-300`
+        }
+      >
+        <AiOutlineArrowUp />
+      </Link> */}
     </div>
   );
 }
